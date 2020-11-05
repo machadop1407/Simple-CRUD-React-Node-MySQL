2ef849093537c7726ed70f14b1bf182a7f99a5d7
Pedro Machado <machadop1407@gmail.com>
2020-11-05 12:01:07 -0300

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,100 @@
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <div className="employees">
+        <button onClick={getEmployees}>Show Employees</button>
+
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <h3>Name: {val.name}</h3>
+              <h3>Age: {val.age}</h3>
+              <h3>Country: {val.country}</h3>
+              <h3>Position: {val.position}</h3>
+              <h3>Wage: {val.wage}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "employeeSystem",
+});
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+});
+
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
